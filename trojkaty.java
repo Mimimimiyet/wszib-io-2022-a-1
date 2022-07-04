2d343fe78e0ea1469895be6185ac8381159e50b1
Mateusz Stanisław Kupiec <makupiec@artemis.wszib.edu.pl>
2022-07-04 23:03:04 +0200

    Długo wyczekiwany Feature: teraz wykrywamy pitagorejskie

--- trojkaty.java
+++ trojkaty.java
@@ -5,10 +5,18 @@
 class Trojkaty {
     /**
      * Główna funkcja programu.
+     * Główna funkcja programu razem z Funkcja do sprawdzenia czy trójkąt o podanych bokach to trójkąt prostokątny
      * @param {float} a - Długość pierwszego boku.
      * @param {float} b - Długość drugiego boku.
      * @param {float} c - Długość trzeciego boku.
      */
+
+               public static boolean czyProstokatny(float a, float b, float c){
+                   if (a * a + b * b == c*c) return true;
+                   if (b * b + c * c == a*a) return true;
+                   if (a * a + c * c == b*b) return true;
+                   return false;
+               }
     public static void jakiTrojkat(float a, float b, float c){
          if (a == b && b == c) {
              System.out.println("Trójkąt równoboczny"); 
@@ -19,6 +27,9 @@ class Trojkaty {
                 if (a == b || b == c || a == c) {
                     System.out.println("Trójkąt równoramienny");
                 }
+		if (czyProstokatny(a, b, c)) {
+                    System.out.println("Trójkąt prostokątny");
+                  }
 
          // TODO: tutaj trzeba bedzie dopisac inne przypadki
     }
@@ -36,16 +47,12 @@ class Trojkaty {
         }
         float a = Float.valueOf(args[0]);
         float b = Float.valueOf(args[1]);
-	float c = Float.valueOf(args[2]);
-
-                if (a < 0 || b < 0 || c < 0) {
+        float c = Float.valueOf(args[2]);
+        
+	if (a < 0 || b < 0 || c < 0) {
                     System.out.println("Długości boków trójkąta muszą być nieujemne!");
                     System.exit(2);
                 }
-
-                jakiTrojkat(a, b, c);
-
-        
         jakiTrojkat(a, b, c);
     }
 }
